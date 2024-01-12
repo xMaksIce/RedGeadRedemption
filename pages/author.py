3442f8667d29aca1bd9747eab4fb1d837e17dbf5
Maksim Kholod <125661351+xMaksIce@users.noreply.github.com>
2024-01-13 00:00:58 +0600

    Delete pages/author.py

--- pages/author.py
+++
@@ -1,5 +0,0 @@
-import streamlit as st
-
-st.header("Разработка Web-приложения для инференса моделей ML и анализа данных")
-st.write("Выполнил: Холод Максим Максимович, ФИТ-221")
-st.image("./data/photo.jpg", width=150)
