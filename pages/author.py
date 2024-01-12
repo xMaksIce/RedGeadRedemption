0efd14094c1cedcfa57e519a0a7704fdb0695c8e
Maksim Kholod <125661351+xMaksIce@users.noreply.github.com>
2024-01-12 22:53:34 +0600

    add pages

---
+++ pages/author.py
@@ -0,0 +1,5 @@
+import streamlit as st
+
+st.header("Разработка Web-приложения для инференса моделей ML и анализа данных")
+st.write("Выполнил: Холод Максим Максимович, ФИТ-221")
+st.image("./data/photo.jpg", width=150)
